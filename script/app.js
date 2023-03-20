8dbe6b4964ff97c81e79af703e8d78b320b82d8b
José Gabriel Quio Sanchez <gabrielquio09@gmail.com>
2023-03-20 13:01:24 -0500

    Solo Estructura Modal HTML y CSS - sin firabase

--- script/app.js
+++ script/app.js
@@ -1,14 +1,37 @@
-let btnAccess = document.querySelector(".header_btn_access");
+//-------------FORMA UNO---------------
+/* let btnAccess = document.querySelector(".header_btn_access");
 let modal = document.querySelector(".modal");
-let btnModalCerrar = document.querySelector(".modal_cerrar");
+let btnModalCerrar = document.querySelector(".modal_close");
 
 
 btnAccess.addEventListener("click",function(ev){
+    ev.preventDefault();
    modal.style.display = "flex"
 
 });
 
 btnModalCerrar.addEventListener("click",function(ev){
+    ev.preventDefault();
     modal.style.display = "none"
-});
+}); */
 
+//FORMA DOS:
+const openModal = document.querySelector(".modal_open");
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".modal_close");
+const speakcardsModal = document.querySelector("#speakcards_modal");
+const goModal = document.querySelector("#go_modal");
+
+function showModal(ev){
+    ev.preventDefault();
+    modal.classList.add("modal--show");
+};
+
+openModal.addEventListener("click",showModal);
+speakcardsModal.addEventListener("click",showModal);
+goModal.addEventListener("click",showModal);
+
+closeModal.addEventListener("click",function(ev){
+    ev.preventDefault();
+   modal.classList.remove("modal--show");
+});
